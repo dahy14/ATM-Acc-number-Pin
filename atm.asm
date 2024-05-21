13706bbc7488173079371dac294f0b0d7aea3276
John Louis Naypes <naypes.15@gmail.com>
2024-05-21 22:17:26 +0800

    Final ATM with masked input

--- atm.asm
+++ atm.asm
@@ -7,14 +7,16 @@ DATA SEGMENT
     IDS2         DW 000AH,000BH,000CH,000DH,000EH,000FH,0010H,0011H,0012H,0013H 
     PASSWORDS1   DB   00H,  01H,  02H,  03H,  04H,  05H,  06H,  07H,  08H,  09H
     PASSWORDS2   DB   0AH,  0BH,  0CH,  0DH,  0EH,  0FH,  01H,  02H,  03H,  04H
-    DATA1        DB '******WELCOME*******',0
-    DATA2        DB 0DH,0AH,'ENTER YOUR ID: ',0
-    DATA3        DB 0DH,0AH,'ENTER YOUR PASSWORD: ',0 
+    DATA1        DB 'W*',0
+    DATA2        DB 0DH,0AH,'ID: ',0
+    DATA3        DB 0DH,0AH,'PW: ',0 
     DATA4        DB 0DH,0AH,'DENIED 0  ',0  
     DATA5        DB 0DH,0AH,'ALLOWED 1 ',0 
-    DATA6        DB '******WELCOME BACK*******',0
+    DATA6        DB '**',0 
+    DATA7        DB '*',0
     IDINPUT      DW 1 DUP (?)
-    PASSINPUT    DB 1 DUP (?)
+    PASSINPUT    DB 1 DUP (?)  
+    CXINPUT      DB 1 DUP (?)
 DATA ENDS
 
 ;***************** 
@@ -53,15 +55,96 @@ L1:   INC  CX
       JMP  L1
       
 
+SCAN_NUM_MASKED PROC
+    PUSH    AX
+    PUSH    BX
+    PUSH    CX
+    PUSH    DX
+    PUSH    SI
+    PUSH    DI
+
+    XOR     CX, CX           ; Clear CX to store the result
+    XOR     BX, BX           ; Clear BX to store the digit count
+
+next_digit:
+    MOV     AH, 00h          ; BIOS keyboard service
+    INT     16h              ; Get character in AL
+
+    CMP     AL, 13           ; Check if Enter key (carriage return)
+    JE      input_done
+
+    CMP     AL, 8            ; Check if Backspace key
+    JE      backspace
+
+    CMP     AL, '0'          ; Check if it's a valid digit
+    JB      next_digit       ; If less than '0', ignore and get the next character
+    CMP     AL, '9'
+    JA      next_digit       ; If greater than '9', ignore and get the next character
+
+    SUB     AL, '0'          ; Convert ASCII to number (AL now holds the digit 0-9)
+    MOV     AH, 0
+    PUSH    AX               ; Save the digit on the stack
+
+    ; Update CX with the new digit
+    MOV     AX, CX
+    MOV     DI, 10
+    MUL     DI               ; AX = CX * 10
+    POP     DI
+    ADD     AX, DI           ; AX = AX + new digit
+    MOV     CX, AX
+
+    ; Print asterisk to mask the digit
+    MOV     AH, 0Eh
+    MOV     AL, '*'
+    INT     10h
+
+    INC     BX               ; Increment the digit count
+    JMP     next_digit
+
+backspace:
+    CMP     BX, 0            ; If no digits, ignore backspace
+    JE      next_digit
+
+    DEC     BX               ; Decrement digit count
+    MOV     AH, 0Eh
+    MOV     AL, 8            ; Backspace
+    INT     10h
+    MOV     AL, ' '
+    INT     10h
+    MOV     AL, 8            ; Backspace again
+    INT     10h
+
+    ; Remove the last digit from CX
+    MOV     AX, CX
+    XOR     DX, DX
+    MOV     DI, 10
+    DIV     DI               ; AX = CX / 10, DX = CX % 10
+    MOV     CX, AX
+    JMP     next_digit
+
+input_done:
+    MOV     CXINPUT, CL      ; Store the result in CXINPUT
+
+    POP     DI
+    POP     SI
+    POP     DX
+    POP     CX
+    POP     BX
+    POP     AX
+    RET
+SCAN_NUM_MASKED ENDP
+
+
 PASS1:LEA  SI,DATA3
       CALL PRINT_STRING        
-      CALL SCAN_NUM
-      MOV  PASSINPUT,CL
+      CALL SCAN_NUM_MASKED
+      MOV  BL, CXINPUT
+      MOV  PASSINPUT, BL
       MOV  AX,DX 
       MOV  DX,0002H
       DIV  DL 
       MOV  SI,AX 
-      MOV  AL,CL
+      MOV  AL,PASSINPUT
       MOV  AH,00H
       CMP  PASSWORDS1[SI],AL
       JNE  ERROR
@@ -69,13 +152,14 @@ PASS1:LEA  SI,DATA3
       
 PASS2:LEA  SI,DATA3
       CALL PRINT_STRING        
-      CALL SCAN_NUM
-      MOV  PASSINPUT,CL
+      CALL SCAN_NUM_MASKED
+      MOV  BL, CXINPUT
+      MOV  PASSINPUT, BL
       MOV  AX,DX 
       MOV  DX,0002H
       DIV  DL 
       MOV  SI,AX 
-      MOV  AL,CL
+      MOV  AL,PASSINPUT
       MOV  AH,00H
       CMP  PASSWORDS2[SI],AL
       JNE  ERROR
@@ -105,8 +189,3 @@ END START
   
 
 ret    
-      
-      
-          
-     
-     
